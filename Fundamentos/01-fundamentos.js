964efb8b49f10baf9fbe3498dc5be44d8283d067
AdhemiR <adhemir.1608@gmail.com>
2024-10-17 20:15:49 -0500

    Fundamentos JS
    Iniciamos con el aprendizaje de JavaScript

---
+++ Fundamentos/01-fundamentos.js
@@ -0,0 +1,24 @@
+//METODO console.log()
+
+console.log("Hola Mundo");
+console.log(2+2);
+
+console.log("Hola a todos! Observen este numero: ", 5 + 18);
+
+//----------------------------------------------------------------
+
+//Mensajes de error por niveles
+
+console.debug("Mensajes de error");
+console.info("Esto es información útil");
+console.warn("Esto es una advertencia");
+console.error("Esto es un error crítico");
+
+//----------------------------------------------------------------
+
+//Mostrar una traza de error
+
+console.trace("¡He llegado aquí!");
+
+
+//----------------------------------------------------------------
